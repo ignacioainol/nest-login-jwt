c3ef9e35176f84df87b1de4e37004ff71c3b1b79
Ignacio Ainol <iggnaxios@gmail.com>
2023-07-04 12:01:17 -0400

    create jwt and register done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
 import { Model } from 'mongoose';
 import * as bcryptjs from 'bcryptjs';
-import { LoginDto } from './dto/login.dto';
+import { LoginDto, RegisterUserDto, CreateUserDto, UpdateAuthDto } from './dto';
 import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
 @Injectable()
 export class AuthService {
 
@@ -39,7 +40,16 @@ export class AuthService {
     }
   }
 
-  async login(loginDto: LoginDto) {
+  async register(registerDto: RegisterUserDto): Promise<LoginResponse> {
+    const user = await this.create(registerDto);
+
+    return {
+      user,
+      token: this.getJwtToken({ id: user._id })
+    }
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
     const { email, password } = loginDto;
 
     const user = await this.userModel.findOne({ email: email });
@@ -56,7 +66,7 @@ export class AuthService {
 
     return {
       user: rest,
-      token: 'ABCJKDJDSF.3438232.DEFDSFS'
+      token: this.getJwtToken({ id: user.id })
     }
   }
 
@@ -76,7 +86,8 @@ export class AuthService {
     return `This action removes a #${id} auth`;
   }
 
-  getJwtToken(payload) {
-
+  getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 }
